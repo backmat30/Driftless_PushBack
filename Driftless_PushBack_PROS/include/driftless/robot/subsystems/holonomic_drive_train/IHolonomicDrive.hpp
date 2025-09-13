aa68b35e1d4a8cc7f94014c253a6916bafe2b620
backmat30 <matthewbackman@outlook.com>
2025-09-12 19:55:36 -0500

    Added initial methods: destructor, init, run, getMotionVector, and setMotionVector

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp
@@ -1,11 +1,22 @@
 #ifndef __I_HOLONOMIC_DRIVE_HPP__
 #define __I_HOLONOMIC_DRIVE_HPP__
 
+#include "driftless/robot/subsystems/holonomic_drive_train/HolonomicMotionVector.hpp"
+
 namespace driftless {
   namespace robot {
     namespace subsystems {
       namespace holonomic_drive_train {
         class IHolonomicDrive {
+          virtual ~IHolonomicDrive() = default;
+
+          virtual void init() = 0;
+
+          virtual void run() = 0;
+
+          virtual HolonomicMotionVector getMotionVector() = 0;
+
+          virtual void setMotionVector(HolonomicMotionVector motionVector) = 0;
         };
       }
     }
