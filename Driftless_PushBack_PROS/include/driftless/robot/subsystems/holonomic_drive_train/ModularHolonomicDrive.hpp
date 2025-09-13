00c2619e1640d2523a8a7056869f6b3f48d53f7e
backmat30 <matthewbackman@outlook.com>
2025-09-13 10:52:55 -0500

    ModularHolonomicDrive now holds drive modules to be controlled

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/ModularHolonomicDrive.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/ModularHolonomicDrive.hpp
@@ -1,7 +1,11 @@
 #ifndef __MODULAR_HOLONOMIC_DRIVE_HPP__
 #define __MODULAR_HOLONOMIC_DRIVE_HPP__
 
+#include <memory>
+#include <vector>
+
 #include "driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp"
+#include "driftless/robot/subsystems/holonomic_drive_train/holonomic_drive_module/IHolonomicDriveModule.hpp"
 
 namespace driftless {
   namespace robot {
@@ -9,6 +13,7 @@ namespace driftless {
       namespace holonomic_drive_train {
         class ModularHolonomicDrive : public IHolonomicDrive {
           private:
+          std::vector<std::unique_ptr<holonomic_drive_module::IHolonomicDriveModule>> m_modules;
           public:
             void init() override;
 
@@ -17,6 +22,8 @@ namespace driftless {
             HolonomicMotionVector getMotionVector() override;
 
             void setMotionVector(HolonomicMotionVector motionVector) override;
+
+            void addModule(std::unique_ptr<holonomic_drive_module::IHolonomicDriveModule>& module);
         };
       }
     }
