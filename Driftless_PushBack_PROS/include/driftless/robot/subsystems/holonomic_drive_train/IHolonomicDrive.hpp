d1fea07fbc790fec97d7d093997fc48359d443a9
backmat30 <matthewbackman@outlook.com>
2025-09-12 19:59:53 -0500

    Documented the IHolonomicDrive interface

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp
@@ -3,19 +3,41 @@
 
 #include "driftless/robot/subsystems/holonomic_drive_train/HolonomicMotionVector.hpp"
 
+/// @brief The namespace for driftless library code
+/// @author Matthew Backman
 namespace driftless {
+
+  /// @brief The namespace for robot code
+  /// @author Matthew Backman
   namespace robot {
+
+    /// @brief The namespace for subsystems code
+    /// @author Matthew Backman
     namespace subsystems {
+
+      /// @brief The namespace for the holonomic drive train subsystem code
+      /// @author Matthew Backman
       namespace holonomic_drive_train {
+
+        /// @brief Interface for holonomic drive train systems
+        /// @author Matthew Backman
         class IHolonomicDrive {
+          public:
+          /// @brief Destroys the holonomic drive object
           virtual ~IHolonomicDrive() = default;
 
+          /// @brief Initializes the holonomic drive object
           virtual void init() = 0;
 
+          /// @brief Runs the holonomic drive object
           virtual void run() = 0;
 
+          /// @brief Gets the motion vector of the holonomic drive train
+          /// @return __HolonomicMotionVector__ the holonomic drive train motion vector
           virtual HolonomicMotionVector getMotionVector() = 0;
 
+          /// @brief Sets the motion vector of the holonomic drive train
+          /// @param motionVector __HolonomicMotionVector__ the desired motion vector
           virtual void setMotionVector(HolonomicMotionVector motionVector) = 0;
         };
       }
