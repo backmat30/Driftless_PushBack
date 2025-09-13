c30e239d8d1ba82e775ad2fc9070c0254c9a5239
backmat30 <matthewbackman@outlook.com>
2025-09-12 19:22:50 -0500

    Added empty interface for holonomic drive trains

---
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp
@@ -0,0 +1,14 @@
+#ifndef __I_HOLONOMIC_DRIVE_HPP__
+#define __I_HOLONOMIC_DRIVE_HPP__
+
+namespace driftless {
+  namespace robot {
+    namespace subsystems {
+      namespace holonomic_drive_train {
+        class IHolonomicDrive {
+        };
+      }
+    }
+  }
+}
+#endif
