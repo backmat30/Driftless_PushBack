ced534f75d3229aa18b8568f2fbd714a83f93f92
backmat30 <matthewbackman@outlook.com>
2025-09-13 11:07:21 -0500

    Defined init, run, setMotionVector, and addModule methods

---
+++ Driftless_PushBack_PROS/src/driftless/robot/subsystems/holonomic_drive_train/ModularHolonomicDrive.cpp
@@ -0,0 +1,25 @@
+#include "driftless/robot/subsystems/holonomic_drive_train/ModularHolonomicDrive.hpp"
+
+namespace driftless::robot::subsystems::holonomic_drive_train {
+  void ModularHolonomicDrive::init() {
+    for (auto& module : m_modules) {
+      if(module) { module->init(); }
+    }
+  }
+
+  void ModularHolonomicDrive::run() {
+    for (auto& module : m_modules) {
+      if(module) { module->run(); }
+    }
+  }
+
+  void ModularHolonomicDrive::setMotionVector(HolonomicMotionVector motionVector) {
+    for (auto& module : m_modules) {
+      if(module) { module->setMotionVector(motionVector); }
+    }
+  }
+
+  void ModularHolonomicDrive::addModule(std::unique_ptr<holonomic_drive_module::IHolonomicDriveModule>& module) {
+    m_modules.push_back(std::move(module));
+  }
+}
