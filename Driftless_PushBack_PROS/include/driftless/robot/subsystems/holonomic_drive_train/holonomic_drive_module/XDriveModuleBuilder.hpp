002221ac4d689d38af0cc189a9c1569558e1adcb
backmat30 <matthewbackman@outlook.com>
2025-09-13 12:26:48 -0500

    Created builder class for XDriveModule objects

---
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/holonomic_drive_module/XDriveModuleBuilder.hpp
@@ -0,0 +1,77 @@
+#ifndef __X_DRIVE_MODULE_BUILDER_HPP__
+#define __X_DRIVE_MODULE_BUILDER_HPP__
+
+#include <memory>
+
+#include "driftless/robot/subsystems/holonomic_drive_train/holonomic_drive_module/XDriveModule.hpp"
+
+/// @brief The namespace for driftless library code
+/// @author Matthew Backman
+namespace driftless {
+
+/// @brief The namespace for robot code
+/// @author Matthew Backman
+namespace robot {
+
+/// @brief The namespace for subsystems code
+/// @author Matthew Backman
+namespace subsystems {
+
+/// @brief The namespace for the holonomic drive train subsystem code
+/// @author Matthew Backman
+namespace holonomic_drive_train {
+
+/// @brief The namespace for the holonomic drive module code
+/// @author Matthew Backman
+namespace holonomic_drive_module {
+
+/// @brief Builder class for creating an XDriveModule
+/// @author Matthew Backman
+class XDriveModuleBuilder {
+ private:
+  /// @brief The motors used to create the module
+  hal::MotorGroup m_motors;
+
+  /// @brief The offset angle of the module
+  double m_angle_offset;
+
+  /// @brief The conversion factor from angular velocity to voltage
+  double m_angular_velocity_to_voltage;
+
+  /// @brief The conversion factor from linear velocity to voltage
+  double m_linear_velocity_to_voltage;
+
+ public:
+  /// @brief Adds a motor to the module
+  /// @param motor __std::unique_ptr<io::IMotor>&__ The motor to add
+  /// @return __XDriveModuleBuilder*__ A pointer to this builder
+  XDriveModuleBuilder* withMotor(std::unique_ptr<io::IMotor>& motor);
+
+  /// @brief Sets the angle offset of the module
+  /// @param angle_offset __double__ The angle offset in radians
+  /// @return __XDriveModuleBuilder*__ A pointer to this builder
+  XDriveModuleBuilder* withAngleOffset(double angle_offset);
+
+  /// @brief Sets the angular velocity to voltage conversion factor
+  /// @param angular_velocity_to_voltage __double__ The conversion factor
+  /// @return __XDriveModuleBuilder*__ A pointer to this builder
+  XDriveModuleBuilder* withAngularVelocityToVoltage(
+      double angular_velocity_to_voltage);
+
+  /// @brief Sets the linear velocity to voltage conversion factor
+  /// @param linear_velocity_to_voltage __double__ The conversion factor
+  /// @return __XDriveModuleBuilder*__ A pointer to this builder
+  XDriveModuleBuilder* withLinearVelocityToVoltage(
+      double linear_velocity_to_voltage);
+
+  /// @brief Builds the XDriveModule
+  /// @return __std::unique_ptr<IHolonomicDriveModule>__ A unique pointer to the
+  /// created XDriveModule
+  std::unique_ptr<IHolonomicDriveModule> build();
+};
+}  // namespace holonomic_drive_module
+}  // namespace holonomic_drive_train
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace driftless
+#endif
