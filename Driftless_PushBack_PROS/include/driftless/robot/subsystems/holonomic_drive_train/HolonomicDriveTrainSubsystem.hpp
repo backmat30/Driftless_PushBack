399e351f92f2fb3706db669c497693bed7c06d01
backmat30 <matthewbackman@outlook.com>
2025-09-14 11:09:37 -0500

    Updated to actually override methods from ASubsystem

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/HolonomicDriveTrainSubsystem.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/holonomic_drive_train/HolonomicDriveTrainSubsystem.hpp
@@ -1,30 +1,31 @@
 #ifndef __HOLONOMIC_DRIVE_TRAIN_SUBSYSTEM_HPP__
 #define __HOLONOMIC_DRIVE_TRAIN_SUBSYSTEM_HPP__
 
-#include <cstdarg>
+#include <functional>
 #include <memory>
 
 #include "driftless/robot/subsystems/ASubsystem.hpp"
 #include "driftless/robot/subsystems/holonomic_drive_train/IHolonomicDrive.hpp"
 namespace driftless {
-  namespace robot {
-    namespace subsystems {
-      namespace holonomic_drive_train {
+namespace robot {
+namespace subsystems {
+namespace holonomic_drive_train {
 
-        class HolonomicDriveTrainSubsystem : public ASubsystem {
-          private:
-          std::unique_ptr<IHolonomicDrive> m_drive_train{};
-         public:
-          HolonomicDriveTrainSubsystem(std::unique_ptr<IHolonomicDrive>& drive_train);
-          void init();
-          void run();
-          void command(ESubsystemCommand command_name, ...);
-          void* state(ESubsystemState state_name);
-        };
+class HolonomicDriveTrainSubsystem : public ASubsystem {
+ private:
+  std::unique_ptr<IHolonomicDrive> m_drive_train{};
 
-      }  // namespace holonomic_drive_train
-    }
-  }
-}
+ public:
+  HolonomicDriveTrainSubsystem(std::unique_ptr<IHolonomicDrive>& drive_train);
+  void init() override;
+  void run() override;
+  void command(ESubsystemCommand command_name, va_list& args) override;
+  void* state(ESubsystemState state_name) override;
+};
+
+}  // namespace holonomic_drive_train
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace driftless
 
 #endif
--- Driftless_PushBack_PROS/src/driftless/robot/subsystems/holonomic_drive_train/HolonomicDriveTrainSubsystem.cpp
+++ Driftless_PushBack_PROS/src/driftless/robot/subsystems/holonomic_drive_train/HolonomicDriveTrainSubsystem.cpp
@@ -10,10 +10,7 @@ void HolonomicDriveTrainSubsystem::init() { m_drive_train->init(); }
 void HolonomicDriveTrainSubsystem::run() { m_drive_train->run(); }
 
 void HolonomicDriveTrainSubsystem::command(ESubsystemCommand command_name,
-                                           ...) {
-  va_list args;
-  va_start(args, command_name);
-
+                                           va_list& args) {
   switch (command_name) {
     case ESubsystemCommand::HOLONOMIC_DRIVE_TRAIN_SET_MOTION_VECTOR: {
       HolonomicMotionVector motion_vector =
@@ -29,8 +26,6 @@ void HolonomicDriveTrainSubsystem::command(ESubsystemCommand command_name,
       break;
     }
   }
-
-  va_end(args);
 }
 void* HolonomicDriveTrainSubsystem::state(ESubsystemState state_name) {
   return nullptr;
