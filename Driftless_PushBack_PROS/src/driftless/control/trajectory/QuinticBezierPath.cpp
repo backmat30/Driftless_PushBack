f7933656f414f97e0179596f52c060912a0e2036
backmat30 <matthewbackman@outlook.com>
2025-09-16 22:03:44 -0500

    bugfix: Fixed an issue with pros not seeing Eigen

--- Driftless_PushBack_PROS/src/driftless/control/trajectory/QuinticBezierPath.cpp
+++ Driftless_PushBack_PROS/src/driftless/control/trajectory/QuinticBezierPath.cpp
@@ -3,7 +3,9 @@
 namespace driftless::control::trajectory {
 QuinticBezierPath::QuinticBezierPath(Point start, Point c0, Point c1, Point c2,
                                      Point c3, Point end) {
-  m_control_points << start, c0, c1, c2, c3, end;
+  m_control_points << start.getX(), start.getY(), c0.getX(), c0.getY(),
+      c1.getX(), c1.getY(), c2.getX(), c2.getY(), c3.getX(), c3.getY(),
+      end.getX(), end.getY();
 
   // Calculate coefficients for the bezier curve
   m_coefficients << 1, -5, 10, -10, 5, -1, 0, 5, -20, 30, -20, 5, 0, 0, 10, -30,
@@ -22,35 +24,36 @@ Point QuinticBezierPath::getPoint(double t) const {
   Eigen::Matrix<double, 1, 6> T;
   T << std::pow(t, 5), std::pow(t, 4), std::pow(t, 3), std::pow(t, 2), t, 1;
   auto result = (T * m_coefficients) * m_control_points;
-  return result(0);
+  return Point{result(0), result(1)};
 }
 
 Point QuinticBezierPath::getDerivative(double t) const {
   Eigen::Matrix<double, 1, 5> T;
   T << std::pow(t, 4), std::pow(t, 3), std::pow(t, 2), t, 1;
   auto result = (T * m_first_derivative_coefficients) * m_control_points;
-  return result(0);
+  return Point{result(0), result(1)};
 }
 
 Point QuinticBezierPath::getSecondDerivative(double t) const {
   Eigen::Matrix<double, 1, 4> T;
   T << std::pow(t, 3), std::pow(t, 2), t, 1;
   auto result = (T * m_second_derivative_coefficients) * m_control_points;
-  return result(0);
+  return Point{result(0), result(1)};
 }
 
 double QuinticBezierPath::getMaxTime() const { return 1.0; }
 
-void QuinticBezierPath::setControlPoint(int index, const Point point) {
+void QuinticBezierPath::setControlPoint(int index, Point point) {
   if (index >= 0 && index < 6) {
-    m_control_points(index) = point;
+    m_control_points(index, 0) = point.getX();
+    m_control_points(index, 1) = point.getY();
   }
 }
 
 Point QuinticBezierPath::getControlPoint(int index) const {
     Point result{};
     if (index >= 0 && index < 6) {
-      result = m_control_points(index);
+      result = Point{m_control_points(index, 0), m_control_points(index, 1)};
     }
     return result;
   }
