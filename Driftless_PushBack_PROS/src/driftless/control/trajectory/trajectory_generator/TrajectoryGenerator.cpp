ea47c9b47f2e82aca3d845cb2a53246970f566f0
backmat30 <matthewbackman@outlook.com>
2025-09-18 19:53:51 -0500

    Defined methods of TrajectoryGenerator

---
+++ Driftless_PushBack_PROS/src/driftless/control/trajectory/trajectory_generator/TrajectoryGenerator.cpp
@@ -0,0 +1,90 @@
+#include "driftless/control/trajectory/trajectory_generator/TrajectoryGenerator.hpp"
+
+namespace driftless::control::trajectory::trajectory_generator {
+TrajectoryGenerator::TrajectoryGenerator(double delta_d) : m_delta_d{delta_d} {}
+
+void TrajectoryGenerator::addConstraint(
+    std::unique_ptr<kinematics::IKinematicConstraint>& constraint) {
+  m_constraints.push_back(constraint);
+}
+
+void TrajectoryGenerator::generateTrajectory(std::unique_ptr<IPath>& path) {
+  double t = 0;
+  m_trajectory.clear();
+  m_trajectory.push_back(TrajectoryPoint{
+      path->getPoint(0).getX(), path->getPoint(0).getY(),
+      std::atan2(path->getDerivative(0).getY(), path->getDerivative(0).getX()),
+      0, 0});
+  TrajectoryPoint last_point{m_trajectory.back()};
+
+  while (t < path->getMaxTime()) {
+    double max_velocity{__DBL_MAX__};
+    for (auto& constraint : m_constraints) {
+      max_velocity = constraint->getMaxVelocity(path, last_point, m_delta_d, t);
+    }
+
+    Point derivative{path->getDerivative(t)};
+    Point second_derivative{path->getSecondDerivative(t)};
+
+    double dt{m_delta_d / (std::sqrt(std::pow(derivative.getX(), 2) +
+                                     std::pow(derivative.getY(), 2)))};
+
+    double curvature{(derivative.getX() * second_derivative.getY() -
+                      derivative.getY() * second_derivative.getX()) /
+                     (std::pow(std::pow(derivative.getX(), 2) +
+                                   std::pow(derivative.getY(), 2),
+                               1.5))};
+
+    last_point =
+        TrajectoryPoint{path->getPoint(t).getX(), path->getPoint(t).getY(),
+                        atan2(derivative.getY(), derivative.getX()),
+                        max_velocity, max_velocity * curvature};
+
+    m_trajectory.push_back(last_point);
+    t += dt;
+  }
+
+  int i = m_trajectory.size() - 1;
+  t = path->getMaxTime();
+  last_point = TrajectoryPoint{
+      path->getPoint(t).getX(), path->getPoint(t).getY(),
+      std::atan2(path->getDerivative(0).getY(), path->getDerivative(0).getX()),
+      0, 0};
+
+  while (t > 0) {
+    if (last_point.m_velocity < m_trajectory[i].m_velocity) {
+      m_trajectory[i] = last_point;
+    }
+
+    double max_velocity{__DBL_MAX__};
+    for (auto& constraint : m_constraints) {
+      max_velocity = constraint->getMaxVelocity(path, last_point, m_delta_d, t);
+    }
+
+    Point derivative{path->getDerivative(t)};
+    Point second_derivative{path->getSecondDerivative(t)};
+
+    double dt{m_delta_d / (std::sqrt(std::pow(derivative.getX(), 2) +
+                                     std::pow(derivative.getY(), 2)))};
+
+    double curvature{(derivative.getX() * second_derivative.getY() -
+                      derivative.getY() * second_derivative.getX()) /
+                     (std::pow(std::pow(derivative.getX(), 2) +
+                                   std::pow(derivative.getY(), 2),
+                               1.5))};
+
+    last_point =
+        TrajectoryPoint{path->getPoint(t).getX(), path->getPoint(t).getY(),
+                        atan2(derivative.getY(), derivative.getX()),
+                        max_velocity, max_velocity * curvature};
+
+    t -= dt;
+    i--;
+  }
+}
+
+std::vector<TrajectoryPoint> TrajectoryGenerator::getTrajectory() {
+  return m_trajectory;
+}
+
+}  // namespace driftless::control::trajectory::trajectory_generator
