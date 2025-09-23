e9c0e63a4a6fbd68b2c4447a5f0362d256563ea1
backmat30 <matthewbackman@outlook.com>
2025-09-23 12:37:04 -0500

    Added HolonomicSpinModifier and SpinContext to allow spin motions to be applied to trajectories

---
+++ Driftless_PushBack_PROS/include/driftless/control/trajectory/trajectory_generator/modifiers/HolonomicSpinModifier.hpp
@@ -0,0 +1,55 @@
+#ifndef __HOLONOMIC_SPIN_MODIFIER_HPP__
+#define __HOLONOMIC_SPIN_MODIFIER_HPP__
+
+#include <vector>
+
+#include "driftless/control/trajectory/SpinContext.hpp"
+#include "driftless/control/trajectory/trajectory_generator/modifiers/ITrajectoryModifier.hpp"
+
+/// @brief Namespace for driftless library code
+/// @author Matthew Backman
+namespace driftless {
+
+/// @brief Namespace for control algorithms
+/// @author Matthew Backman
+namespace control {
+
+/// @brief Namespace for trajectory generation and following
+/// @author Matthew Backman
+namespace trajectory {
+
+/// @brief namespace for building trajectories
+/// @author Matthew Backman
+namespace trajectory_generator {
+
+/// @brief Namespace for trajectory modifiers
+/// @author Matthew Backman
+namespace modifiers {
+
+/// @brief Trajectory modifier to apply spins along the path for a holonomic
+/// drive train
+/// @author Matthew Backman
+class HolonomicSpinModifier : public ITrajectoryModifier {
+ private:
+  /// @brief The list of spins to apply to a trajectory
+  std::vector<SpinContext> m_spins{};
+
+ public:
+  /// @brief Constructs a HolonomicSpinModifier object
+  HolonomicSpinModifier() = default;
+
+  /// @brief Applies the modifier to the provided trajectory
+  /// @param trajectory __std::vector<TrajectoryPoint>&__ The trajectory to
+  /// modify
+  void applyModifier(std::vector<TrajectoryPoint>& trajectory) override;
+
+  /// @brief Sets the spins to use
+  /// @param spins __std::vector<SpinContext>&__ The spins to use
+  void setSpins(std::vector<SpinContext>& spins);
+};
+}  // namespace modifiers
+}  // namespace trajectory_generator
+}  // namespace trajectory
+}  // namespace control
+}  // namespace driftless
+#endif
