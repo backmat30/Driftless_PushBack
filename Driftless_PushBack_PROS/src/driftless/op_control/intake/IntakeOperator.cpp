411191f9e8b25ac4cf2732a5b3c24cc06b6ea336
backmat30 <matthewbackman@outlook.com>
2025-09-25 18:53:41 -0500

    Intake uses correct voltages in op control

--- Driftless_PushBack_PROS/src/driftless/op_control/intake/IntakeOperator.cpp
+++ Driftless_PushBack_PROS/src/driftless/op_control/intake/IntakeOperator.cpp
@@ -10,26 +10,34 @@ void IntakeOperator::updateIntakeSplit(EControllerDigital front_intake_in,
   bool run_back_intake_in{m_controller->getDigital(back_intake_in)};
   bool toggle_back_arms{m_controller->getNewDigital(back_arms_toggle)};
 
+  double front_voltage{};
+  double intermediary_voltage{};
+  double back_voltage{};
+
   if (run_back_intake_in) {
-    m_robot->sendCommand(
-        robot::subsystems::ESubsystem::INTAKE,
-        robot::subsystems::ESubsystemCommand::INTAKE_SET_BACK_VOLTAGE, 12.0);
+    back_voltage = -12.0;
+    intermediary_voltage = 12.0;
+
   } else if (run_front_intake_in) {
-    m_robot->sendCommand(
-        robot::subsystems::ESubsystem::INTAKE,
-        robot::subsystems::ESubsystemCommand::INTAKE_SET_FRONT_VOLTAGE, 12.0);
-    m_robot->sendCommand(
-        robot::subsystems::ESubsystem::INTAKE,
-        robot::subsystems::ESubsystemCommand::INTAKE_SET_INTERMEDIARY_VOLTAGE,
-        12.0);
-    m_robot->sendCommand(
-        robot::subsystems::ESubsystem::INTAKE,
-        robot::subsystems::ESubsystemCommand::INTAKE_SET_BACK_VOLTAGE, 12.0);
+    front_voltage = 12.0;
+    intermediary_voltage = 12.0;
+    back_voltage = 12.0;
+  
   } else if (run_front_intake_out) {
-    m_robot->sendCommand(
-        robot::subsystems::ESubsystem::INTAKE,
-        robot::subsystems::ESubsystemCommand::INTAKE_SET_FRONT_VOLTAGE, 12.0);
+    front_voltage = -12.0;
   }
+
+  m_robot->sendCommand(
+      robot::subsystems::ESubsystem::INTAKE,
+      robot::subsystems::ESubsystemCommand::INTAKE_SET_FRONT_VOLTAGE, front_voltage);
+  m_robot->sendCommand(
+      robot::subsystems::ESubsystem::INTAKE,
+      robot::subsystems::ESubsystemCommand::INTAKE_SET_INTERMEDIARY_VOLTAGE,
+      intermediary_voltage);
+  m_robot->sendCommand(
+      robot::subsystems::ESubsystem::INTAKE,
+      robot::subsystems::ESubsystemCommand::INTAKE_SET_BACK_VOLTAGE,
+      back_voltage);
 
   if (toggle_back_arms) {
     bool is_intake_deployed{*static_cast<bool*>(m_robot->getState(
@@ -53,12 +61,18 @@ IntakeOperator::IntakeOperator(
     const std::shared_ptr<robot::Robot>& robot)
     : m_controller{controller}, m_robot{robot} {}
 
-void IntakeOperator::update(const std::unique_ptr<profiles::IProfile>& profile) {
-  EControllerDigital front_intake_in{profile->getDigitalControlMapping(EControl::INTAKE_FRONT_RUN_IN)};
-  EControllerDigital front_intake_out{profile->getDigitalControlMapping(EControl::INTAKE_FRONT_RUN_OUT)};
-  EControllerDigital back_intake_in{profile->getDigitalControlMapping(EControl::INTAKE_BACK_RUN_IN)};
-  EControllerDigital back_arms_toggle{profile->getDigitalControlMapping(EControl::INTAKE_BACK_TOGGLE_ARMS)};
+void IntakeOperator::update(
+    const std::unique_ptr<profiles::IProfile>& profile) {
+  EControllerDigital front_intake_in{
+      profile->getDigitalControlMapping(EControl::INTAKE_FRONT_RUN_IN)};
+  EControllerDigital front_intake_out{
+      profile->getDigitalControlMapping(EControl::INTAKE_FRONT_RUN_OUT)};
+  EControllerDigital back_intake_in{
+      profile->getDigitalControlMapping(EControl::INTAKE_BACK_RUN_IN)};
+  EControllerDigital back_arms_toggle{
+      profile->getDigitalControlMapping(EControl::INTAKE_BACK_TOGGLE_ARMS)};
 
-  updateIntakeSplit(front_intake_in, front_intake_out, back_intake_in, back_arms_toggle);
+  updateIntakeSplit(front_intake_in, front_intake_out, back_intake_in,
+                    back_arms_toggle);
 }
 }  // namespace driftless::op_control::intake
