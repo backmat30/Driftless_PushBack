6fb9504d25b4d8a41d0baa9646f82d41ee5c25cd
backmat30 <matthewbackman@outlook.com>
2025-09-26 17:03:38 -0500

    Updated formatting on IHood

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/hood/IHood.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/hood/IHood.hpp
@@ -13,47 +13,47 @@ namespace robot {
 /// @author Matthew Backman
 namespace subsystems {
 
-      /// @brief Namespace for the hood subsystem
-      /// @author Matthew Backman
-      namespace hood {
+/// @brief Namespace for the hood subsystem
+/// @author Matthew Backman
+namespace hood {
 
-        /// @brief Interface for a generic hood object
-        /// @author Matthew Backman
-        class IHood {
-          public:
-          /// @brief Destroys the hood object
-          virtual ~IHood() = default;
+/// @brief Interface for a generic hood object
+/// @author Matthew Backman
+class IHood {
+ public:
+  /// @brief Destroys the hood object
+  virtual ~IHood() = default;
 
-          /// @brief Initializes the hood
-          virtual void init() = 0;
+  /// @brief Initializes the hood
+  virtual void init() = 0;
 
-          /// @brief Runs the hood
-          virtual void run() = 0;
+  /// @brief Runs the hood
+  virtual void run() = 0;
 
-          /// @brief Sets the voltage of the hood motors
-          /// @param voltage __double__ The voltage
-          virtual void setVoltage(double voltage) = 0;
+  /// @brief Sets the voltage of the hood motors
+  /// @param voltage __double__ The voltage
+  virtual void setVoltage(double voltage) = 0;
 
-          /// @brief Opens the gate at the end of the hood
-          virtual void open() = 0;
+  /// @brief Opens the gate at the end of the hood
+  virtual void open() = 0;
 
-          /// @brief Closes the gate at the end of the hood
-          virtual void close() = 0;
+  /// @brief Closes the gate at the end of the hood
+  virtual void close() = 0;
 
-          /// @brief Toggles the gate between open and closed
-          virtual void toggleOpen() = 0;
+  /// @brief Toggles the gate between open and closed
+  virtual void toggleOpen() = 0;
 
-          /// @brief Raises the hood
-          virtual void raise() = 0;
+  /// @brief Raises the hood
+  virtual void raise() = 0;
 
-          /// @brief Lowers the hood
-          virtual void lower() = 0;
+  /// @brief Lowers the hood
+  virtual void lower() = 0;
 
-          /// @brief Toggles the hood between raised and lowered
-          virtual void toggleRaised() = 0;
-        };
-      }
-    }
-  }
-}
+  /// @brief Toggles the hood between raised and lowered
+  virtual void toggleRaised() = 0;
+};
+}  // namespace hood
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace driftless
 #endif
