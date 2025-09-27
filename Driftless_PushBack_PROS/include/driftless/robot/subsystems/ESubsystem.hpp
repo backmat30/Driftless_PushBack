4b6fa17b0719d4dd2515d81449339b1fa084947c
backmat30 <matthewbackman@outlook.com>
2025-09-26 19:29:47 -0500

    Added the hood to the list of subsystems in ESubsystem

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/ESubsystem.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/ESubsystem.hpp
@@ -15,7 +15,7 @@ namespace subsystems {
 
 /// @brief The enum class for subsystems
 /// @author Matthew Backman
-enum class ESubsystem { HOLONOMIC_DRIVE_TRAIN, TANK_DRIVE_TRAIN, ODOMETRY, INTAKE };
+enum class ESubsystem { HOLONOMIC_DRIVE_TRAIN, TANK_DRIVE_TRAIN, ODOMETRY, INTAKE, HOOD };
 }  // namespace subsystems
 }  // namespace robot
 }  // namespace driftless
