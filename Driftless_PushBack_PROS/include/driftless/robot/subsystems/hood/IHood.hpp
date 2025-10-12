e4434c314dc1f43ae7ce21092bf2ada2b2434896
backmat30 <matthewbackman@outlook.com>
2025-10-12 16:17:23 -0500

    Added methods for descore mech to IHood

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/hood/IHood.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/hood/IHood.hpp
@@ -52,6 +52,15 @@ class IHood {
   /// @brief Toggles the hood between raised and lowered
   virtual void toggleRaised() = 0;
 
+  /// @brief Extends the descore mechanism
+  virtual void extendDescore() = 0;
+
+  /// @brief Retracts the descore mechanism
+  virtual void retractDescore() = 0;
+
+  /// @brief Toggles the descore mechanism between extended and retracted
+  virtual void toggleDescore() = 0;
+
   /// @brief Checks if the hood is raised
   /// @return __bool__ True if the hood is raised, false otherwise
   virtual bool isRaised() = 0;
