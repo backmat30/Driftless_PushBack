00fcaef7dfa90511cc7106b01e058502fd3d95ac
backmat30 <matthewbackman@outlook.com>
2025-11-05 18:09:55 -0600

    Added class for a package of information

---
+++ Driftless_PushBack_PROS/src/driftless/serial_protocol/Package.cpp
@@ -0,0 +1,22 @@
+#include "driftless/serial_protocol/Package.hpp"
+
+namespace driftless::serial_protocol {
+void Package::addPacket(const Packet& packet) {
+  m_packets.push_back(packet);
+}
+
+void Package::addPacket(const ESerialKey key, const std::string& value) {
+  Packet packet{key, value};
+  m_packets.push_back(packet);
+}
+
+std::string Package::getSerializedPackage() const {
+  std::string output_string{};
+  output_string += static_cast<char>(m_packets.size());
+  for(Packet packet : m_packets) {
+    output_string += static_cast<char>(packet.key);
+    output_string += packet.value;
+  }
+  return output_string;
+}
+}
