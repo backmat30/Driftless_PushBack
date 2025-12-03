fce7979e6dceb602d096e4de4037f7d0636102e1
backmat30 <matthewbackman@outlook.com>
2025-12-02 21:51:59 -0600

    Added builder class for holonomicGoToPoint objects

---
+++ Driftless_PushBack_PROS/include/driftless/control/motion/PIDHolonomicGoToPointBuilder.hpp
@@ -0,0 +1,90 @@
+#ifndef __PID_HOLONOMIC_GO_TO_POINT_BUILDER_HPP__
+#define __PID_HOLONOMIC_GO_TO_POINT_BUILDER_HPP__
+
+#include "driftless/control/motion/PIDHolonomicGoToPoint.hpp"
+
+/// @brief Namespace for driftless library code
+/// @author Matthew Backman
+namespace driftless {
+
+/// @brief Namespace for control algorithms
+/// @author Matthew Backman
+namespace control {
+
+/// @brief Namespace for direct motion control
+/// @author Matthew Backman
+namespace motion {
+
+  /// @brief Builder class for PIDHolonomicGoToPoint objects
+  class PIDHolonomicGoToPointBuilder {
+    private:
+    std::unique_ptr<rtos::IDelayer> m_delayer{};
+
+    std::unique_ptr<rtos::IMutex> m_mutex{};
+
+    std::unique_ptr<rtos::ITask> m_task{};
+
+    PID m_x_pid{};
+
+    PID m_y_pid{};
+
+    PID m_rotational_pid{};
+
+    double m_distance_tolerance{};
+
+    double m_velocity_tolerance{};
+
+    double m_angular_tolerance{};
+
+    public:
+    /// @brief Sets the delayer for the control
+    /// @param delayer __std::unique_ptr<rtos::IDelayer>&__ The delayer to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withDelayer(std::unique_ptr<rtos::IDelayer>& delayer);
+
+    /// @brief Sets the mutex for the control
+    /// @param mutex __std::unique_ptr<rtos::IMutex>&__ The mutex to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withMutex(std::unique_ptr<rtos::IMutex>& mutex);
+
+    /// @brief Sets the task for the control
+    /// @param task __std::unique_ptr<rtos::ITask>&__ The task to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withTask(std::unique_ptr<rtos::ITask>& task);
+
+    /// @brief Sets the x PID for the control
+    /// @param pid __PID&__ The x PID to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withXPid(const PID& pid);
+
+    /// @brief Sets the y PID for the control
+    /// @param pid __PID&__ The y PID to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withYPid(const PID& pid);
+
+    /// @brief Sets the rotational PID for the control
+    /// @param pid __PID&__ The rotational PID to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withRotationalPid(const PID& pid);
+
+    /// @brief Sets the distance tolerance for the control
+    /// @param tolerance __double__ The distance tolerance to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withDistanceTolerance(double tolerance);
+
+    /// @brief Sets the velocity tolerance for the control
+    /// @param tolerance __double__ The velocity tolerance to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withVelocityTolerance(double tolerance);
+
+    /// @brief Sets the angular tolerance for the control
+    /// @param tolerance __double__ The angular tolerance to use
+    /// @return __PIDHolonomicGoToPointBuilder*__ Pointer to the builder
+    PIDHolonomicGoToPointBuilder* withAngularTolerance(double tolerance);
+
+    /// @brief Builds a PIDHolonomicGoToPoint object
+    /// @return __std::unique_ptr<PIDHolonomicGoToPoint>__ The built object
+    std::unique_ptr<PIDHolonomicGoToPoint> build();
+  };
+}}}
+#endif
---
+++ Driftless_PushBack_PROS/src/driftless/control/motion/PIDHolonomicGoToPointBuilder.cpp
@@ -0,0 +1,73 @@
+#include "driftless/control/motion/PIDHolonomicGoToPointBuilder.hpp"
+
+namespace driftless::control::motion {
+PIDHolonomicGoToPointBuilder* PIDHolonomicGoToPointBuilder::withDelayer(
+    std::unique_ptr<rtos::IDelayer>& delayer) {
+  m_delayer = std::move(delayer);
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder* PIDHolonomicGoToPointBuilder::withMutex(
+    std::unique_ptr<rtos::IMutex>& mutex) {
+  m_mutex = std::move(mutex);
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder* PIDHolonomicGoToPointBuilder::withTask(
+    std::unique_ptr<rtos::ITask>& task) {
+  m_task = std::move(task);
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder* PIDHolonomicGoToPointBuilder::withXPid(
+    const PID& pid) {
+  m_x_pid = pid;
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder* PIDHolonomicGoToPointBuilder::withYPid(
+    const PID& pid) {
+  m_y_pid = pid;
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder* PIDHolonomicGoToPointBuilder::withRotationalPid(
+    const PID& pid) {
+  m_rotational_pid = pid;
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder*
+PIDHolonomicGoToPointBuilder::withDistanceTolerance(double tolerance) {
+  m_distance_tolerance = tolerance;
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder*
+PIDHolonomicGoToPointBuilder::withVelocityTolerance(double tolerance) {
+  m_velocity_tolerance = tolerance;
+  return this;
+}
+
+PIDHolonomicGoToPointBuilder*
+PIDHolonomicGoToPointBuilder::withAngularTolerance(double tolerance) {
+  m_angular_tolerance = tolerance;
+  return this;
+}
+
+std::unique_ptr<PIDHolonomicGoToPoint> PIDHolonomicGoToPointBuilder::build() {
+  std::unique_ptr<PIDHolonomicGoToPoint> go_to_point{
+      std::make_unique<PIDHolonomicGoToPoint>()};
+
+  go_to_point->setDelayer(m_delayer);
+  go_to_point->setMutex(m_mutex);
+  go_to_point->setXPID(m_x_pid);
+  go_to_point->setYPID(m_y_pid);
+  go_to_point->setRotationalPID(m_rotational_pid);
+  go_to_point->setDistanceTolerance(m_distance_tolerance);
+  go_to_point->setVelocityTolerance(m_velocity_tolerance);
+  go_to_point->setAngularTolerance(m_angular_tolerance);
+
+  return go_to_point;
+}
+}  // namespace driftless::control::motion
