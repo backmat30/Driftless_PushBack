3738a0752e96c1abc47f45cb66ebcac94d68262b
backmat30 <matthewbackman@outlook.com>
2025-12-11 17:39:26 -0600

    Fixed drive motor ports for the orange robot

--- Driftless_PushBack_PROS/include/driftless/config/OrangeConfig.hpp
+++ Driftless_PushBack_PROS/include/driftless/config/OrangeConfig.hpp
@@ -160,10 +160,10 @@ class OrangeConfig : public IConfig {
   static constexpr int DRIVE_FRONT_LEFT_BOTTOM_PORT{-12};
   static constexpr int DRIVE_FRONT_RIGHT_TOP_PORT{15};
   static constexpr int DRIVE_FRONT_RIGHT_BOTTOM_PORT{-13};
-  static constexpr int DRIVE_BACK_LEFT_TOP_PORT{19};
-  static constexpr int DRIVE_BACK_LEFT_BOTTOM_PORT{-20};
-  static constexpr int DRIVE_BACK_RIGHT_TOP_PORT{18};
-  static constexpr int DRIVE_BACK_RIGHT_BOTTOM_PORT{-17};
+  static constexpr int DRIVE_BACK_LEFT_TOP_PORT{20};
+  static constexpr int DRIVE_BACK_LEFT_BOTTOM_PORT{-19};
+  static constexpr int DRIVE_BACK_RIGHT_TOP_PORT{17};
+  static constexpr int DRIVE_BACK_RIGHT_BOTTOM_PORT{-18};
 
   // ## INTAKE MOTORS ##
 
