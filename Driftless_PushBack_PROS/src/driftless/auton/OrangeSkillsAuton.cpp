49f225d9515b3c86aca9c7ea238750be50382462
backmat30 <matthewbackman@outlook.com>
2025-12-11 18:00:17 -0600

    Added an auton class for the orange skills route

---
+++ Driftless_PushBack_PROS/src/driftless/auton/OrangeSkillsAuton.cpp
@@ -0,0 +1,39 @@
+#include "driftless/auton/OrangeSkillsAuton.hpp"
+
+namespace driftless::auton {
+OrangeSkillsAuton::OrangeSkillsAuton() : AAuton("Orange_Skills") {}
+
+void OrangeSkillsAuton::init(
+    std::shared_ptr<robot::Robot>& robot,
+    std::shared_ptr<control::ControlSystem>& control_system) {
+  m_robot = robot;
+  m_control_system = control_system;
+}
+
+void OrangeSkillsAuton::run(
+    std::shared_ptr<driftless::robot::Robot>& robot,
+    std::shared_ptr<driftless::control::ControlSystem>& control_system,
+    std::shared_ptr<driftless::alliance::IAlliance>& alliance,
+    std::shared_ptr<rtos::IClock>& clock,
+    std::unique_ptr<rtos::IDelayer>& delayer) {
+  m_robot = robot;
+  m_control_system = control_system;
+  m_alliance = alliance;
+  m_clock = clock;
+  m_delayer = delayer->clone();
+
+  // start auton
+  uint32_t start_time{getTime()};
+  setOdomPosition(68.25, 25.0, 0);
+
+  // leave at end
+  stopMotion();
+  intakeFront();
+  hoodBumpUp();
+  hoodOpenDoor();
+  retractBackIntakeArms();
+  retractRake();
+  pros::screen::print(pros::E_TEXT_LARGE_CENTER, 8, "Runtime: %7.2f",
+                      (getTime() - start_time) / 1000.0);
+}
+}  // namespace driftless::auton
