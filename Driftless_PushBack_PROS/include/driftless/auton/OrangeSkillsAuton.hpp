43e0beca57301695eed46c192156835bbd2d591f
backmat30 <matthewbackman@outlook.com>
2026-02-19 21:39:51 -0600

    Orange skills, still needs some slight tweaks for reliability

--- Driftless_PushBack_PROS/include/driftless/auton/OrangeSkillsAuton.hpp
+++ Driftless_PushBack_PROS/include/driftless/auton/OrangeSkillsAuton.hpp
@@ -18,37 +18,51 @@ class OrangeSkillsAuton : public AAuton {
  private:
   static constexpr double MAX_VELOCITY{80.0};
 
-  control::Point middle_blues_lineup_1{128.0, 32.0, -M_PI / 2.0};
+  control::Point middle_blues_lineup_1{130.0, 36.0, M_PI};
 
-  control::Point middle_blues_lineup_2{128.0, 77.5, M_PI};
+  control::Point middle_blues_lineup_2{128.0, 79.0, M_PI};
 
-  control::Point middle_blues{118.0, 77.5, M_PI};
+  control::Point middle_blues{118.0, 78.5, M_PI};
 
-  control::Point back_matchload_lineup{96.0, 95.0, -M_PI * 3.0 / 4.0};
+  control::Point back_matchload_lineup{96.0, 95.0, M_PI};
 
-  control::Point back_matchload_lineup_2{118.0, 116.0, -M_PI * 3.0 / 4.0};
+  control::Point back_matchload_lineup_2{118.0, 116.0, M_PI * 3.0 / 4.0};
 
-  control::Point back_matchload{120.0, 134.0, -M_PI / 2.0};
+  control::Point back_matchload{118.0, 134.0, -M_PI / 2.0};
 
-  control::Point back_long_goal{120.0, 96.0, -M_PI / 2.0};
+  control::Point back_long_goal{121.0, 96.0, -M_PI / 2.0};
 
   control::Point park_zone_blocks_lineup{70.0, 94.0, M_PI / 2.0};
 
-  control::Point park_zone_blocks{73.0, 122.0, M_PI / 2.0};
+  control::Point park_zone_blocks{70.0, 121.0, M_PI / 2.0};
 
-  control::Point wall_blocks_start{134.0, 128.0, -M_PI / 6.0};
+  control::Point wall_blocks_lineup{100.0, 120.0, 0.0};
 
-  control::Point wall_blocks_end{135.0, 64.0, -M_PI / 6.0};
+  control::Point wall_blocks_start{134.0, 128.0, -M_PI / 4.0};
+
+  control::Point wall_blocks_end{134.0, 64.0, -M_PI / 4.0};
 
   control::Point middle_reds{118.0, 64.0, M_PI};
 
   control::Point middle_goal_lineup{96.0, 48.0, M_PI * 3.0 / 4.0};
 
-  control::Point middle_goal{80.0, 59.0, M_PI * 3.0 / 4.0};
+  control::Point middle_goal{83.0, 59.0, M_PI * 3.0 / 4.0};
 
-  control::Point front_match_load{120.0, 12.0, M_PI / 2.0};
+  control::Point front_wall_blocks_lineup{90.0, 28.0, M_PI * 3.0 / 4.0};
 
-  control::Point front_right_long_goal{120.0, 44.0, M_PI / 2.0};
+  control::Point front_wall_blocks_start{132.0, 12.0, M_PI / 4.0};
+
+  control::Point front_wall_blocks_end{134.0, 50.0, M_PI / 4.0};
+
+  control::Point front_match_load_lineup{118.0, 36.0, M_PI / 2.0};
+
+  control::Point front_match_load{118.0, 10.0, M_PI / 2.0};
+
+  control::Point front_right_long_goal{118.0, 44.0, M_PI / 2.0};
+
+  control::Point park_lineup{92.0, 12.0, 0.0};
+
+  control::Point park{72.0, 10.0, 0.0};
 
  public:
   /// @brief Constructs a new orange skills auton object
--- Driftless_PushBack_PROS/src/driftless/auton/OrangeSkillsAuton.cpp
+++ Driftless_PushBack_PROS/src/driftless/auton/OrangeSkillsAuton.cpp
@@ -27,27 +27,25 @@ void OrangeSkillsAuton::run(
   setOdomPosition(88.0, 20.0, M_PI);
 
   // head to the blues under the long goal
-  startColorSort(alliance::EAlliance::BLUE);
   goToPoint(middle_blues_lineup_1, MAX_VELOCITY);
-  waitForGoToPoint(middle_blues_lineup_1, 10.0, 3000);
+  waitForGoToPoint(middle_blues_lineup_1, 14.0, 3000);
   setGoToPointVelocity(MAX_VELOCITY / 3.0);
-  delay(150);
-  goToPoint(middle_blues_lineup_2, MAX_VELOCITY / 1.5);
-  waitForGoToPoint(middle_blues_lineup_2, 16.0, 3000);
-  setGoToPointVelocity(MAX_VELOCITY / 3.0);
+  waitForGoToPoint(middle_blues_lineup_1, 14.0, 3000);
+  goToPoint(middle_blues_lineup_2, MAX_VELOCITY);
+  waitForGoToPoint(middle_blues_lineup_2, 18.0, 3000);
+  setGoToPointVelocity(MAX_VELOCITY / 5.0);
   waitForGoToPoint(middle_blues_lineup_2, 2.0, 2000);
   deployRake();
   intakeFront();
-  goToPoint(middle_blues, MAX_VELOCITY / 2.5);
+  goToPoint(middle_blues, MAX_VELOCITY / 2.0);
   waitForGoToPoint(middle_blues, 2.0, 3000);
 
   // go to the first matchload on blue side
-  goToPoint(back_matchload_lineup, MAX_VELOCITY);
+  goToPoint(back_matchload_lineup, MAX_VELOCITY / 2.0);
   waitForGoToPoint(back_matchload_lineup, 8.0, 3000);
-  goToPoint(back_matchload_lineup_2, MAX_VELOCITY / 3.0);
+  goToPoint(back_matchload_lineup_2, MAX_VELOCITY / 2.0);
   waitForGoToPoint(back_matchload_lineup_2, 2.0, 3000);
   goToPoint(back_matchload, MAX_VELOCITY);
-  pauseColorSort();
   waitForGoToPoint(back_matchload, 16.0, 3000);
   setGoToPointVelocity(MAX_VELOCITY / 3.0);
   waitForGoToPoint(back_matchload, 2.0, 1500);
@@ -60,11 +58,12 @@ void OrangeSkillsAuton::run(
 
   // score on the goal
   goToPoint(back_long_goal, MAX_VELOCITY / 2.0);
-  waitForGoToPoint(back_long_goal, 12.0, 1500);
   intakeFront();
   hoodRaise();
-  setGoToPointVelocity(MAX_VELOCITY / 3.0);
+  waitForGoToPoint(back_long_goal, 16.0, 1500);
+  setGoToPointVelocity(MAX_VELOCITY / 4.0);
   waitForGoToPoint(back_long_goal, 2.0, 1500);
+  stopMotion();
   hoodOpenDoor();
   retractBackIntakeArms();
 
@@ -79,14 +78,15 @@ void OrangeSkillsAuton::run(
   delay(250);
 
   // go to blue park zone for blocks
+  startColorSort(alliance::EAlliance::RED);
   goToPoint(park_zone_blocks_lineup, MAX_VELOCITY);
   waitForGoToPoint(park_zone_blocks_lineup, 18.0, 2000);
   setGoToPointVelocity(MAX_VELOCITY / 2.5);
   waitForGoToPoint(park_zone_blocks_lineup, 4.0, 2000);
   goToPoint(park_zone_blocks, MAX_VELOCITY / 2.0);
-  waitForGoToPoint(park_zone_blocks, 12.0, 1000);
+  waitForGoToPoint(park_zone_blocks, 15.0, 1000);
   setGoToPointVelocity(MAX_VELOCITY / 5.0);
-  waitForGoToPoint(park_zone_blocks, 2.0, 1750);
+  waitForGoToPoint(park_zone_blocks, 3.0, 2000);
   deployRake();
   delay(500);
   intakeFront();
@@ -94,11 +94,13 @@ void OrangeSkillsAuton::run(
   waitForGoToPoint(park_zone_blocks_lineup, 2.0, 1750);
 
   // grab the red blocks against the wall
-  goToPoint(wall_blocks_start, MAX_VELOCITY);
-  waitForGoToPoint(wall_blocks_start, 22.0, 3000);
+  goToPoint(wall_blocks_lineup, MAX_VELOCITY);
+  waitForGoToPoint(wall_blocks_lineup, 8.0, 2000);
+  goToPoint(wall_blocks_start, MAX_VELOCITY / 1.5);
+  waitForGoToPoint(wall_blocks_start, 24.0, 3000);
   retractRake();
   setGoToPointVelocity(MAX_VELOCITY / 3.0);
-  waitForGoToPoint(wall_blocks_start, 3.0, 3000);
+  waitForGoToPoint(wall_blocks_start, 3.0, 2500);
   goToPoint(wall_blocks_end, MAX_VELOCITY / 3.0);
   waitForGoToPoint(wall_blocks_end, 4.0, 4000);
 
@@ -107,21 +109,35 @@ void OrangeSkillsAuton::run(
   delay(500);
   goToPoint(middle_reds, MAX_VELOCITY / 3.0);
   waitForGoToPoint(middle_reds, 4.0, 1000);
-  deployRake();
-  waitForGoToPoint(middle_reds, 2.0, 1000);
 
   // deposit blocks in middle goal
   goToPoint(middle_goal_lineup, MAX_VELOCITY / 2.0);
   waitForGoToPoint(middle_goal_lineup, 2.0, 2000);
-  retractRake();
   goToPoint(middle_goal, MAX_VELOCITY / 3.5);
   waitForGoToPoint(middle_goal, 2.0, 3000);
   outtakeFront();
   hoodSetVoltage(-12.0);
+  pauseColorSort();
 
   delay(2500);
 
+  // go to the blue wall blocks
+  goToPoint(front_wall_blocks_lineup, MAX_VELOCITY);
+  waitForGoToPoint(front_wall_blocks_lineup, 8.0, 2000);
+  intakeFront();
+  goToPoint(front_wall_blocks_start, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(front_wall_blocks_start, 10.0, 2000);
+  setGoToPointVelocity(MAX_VELOCITY / 5.0);
+  waitForGoToPoint(front_wall_blocks_start, 2.0, 2000);
+  goToPoint(front_wall_blocks_end, MAX_VELOCITY / 3.0);
+  waitForGoToPoint(front_wall_blocks_end, 2.0, 4000);
+  delay(250);
+  goToPoint(front_wall_blocks_start, MAX_VELOCITY / 2.0);
+  delay(750);
+
   // go to front matchloader
+  goToPoint(front_match_load_lineup, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(front_match_load_lineup, 4.0, 2000);
   goToPoint(front_match_load, MAX_VELOCITY / 1.25);
   waitForGoToPoint(front_match_load, 16.0, 4000);
   setGoToPointVelocity(MAX_VELOCITY / 3.0);
@@ -137,11 +153,24 @@ void OrangeSkillsAuton::run(
   retractBackIntakeArms();
   intakeStop();
   hoodRaise();
-  waitForGoToPoint(front_right_long_goal, 2.0, 3000);
+  waitForGoToPoint(front_right_long_goal, 2.0, 2500);
+  stopMotion();
   intakeFront();
   hoodOpenDoor();
 
   delay(3000);
+
+  // park
+  goToPoint(park_lineup, MAX_VELOCITY);
+  waitForGoToPoint(park_lineup, 4.0, 3000);
+  hoodLower();
+  hoodCloseDoor();
+  intakeStop();
+  goToPoint(park, MAX_VELOCITY / 2.0);
+
+  pros::screen::print(pros::E_TEXT_LARGE_CENTER, 8, "Runtime: %7.2f",
+                      (getTime() - start_time) / 1000.0);
+  delayUntil(start_time + 60000);
 
   // leave at end
   stopMotion();
@@ -150,7 +179,5 @@ void OrangeSkillsAuton::run(
   hoodOpenDoor();
   retractBackIntakeArms();
   retractRake();
-  pros::screen::print(pros::E_TEXT_LARGE_CENTER, 8, "Runtime: %7.2f",
-                      (getTime() - start_time) / 1000.0);
 }
 }  // namespace driftless::auton
