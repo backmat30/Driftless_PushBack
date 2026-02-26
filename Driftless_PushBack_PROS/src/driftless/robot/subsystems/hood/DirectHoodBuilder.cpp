d7d86c3ab0e327ff5fea288d1c637adee34c0da6
backmat30 <matthewbackman@outlook.com>
2026-02-25 19:12:11 -0600

    Updated the directHood builder for the new 3 state descore

--- Driftless_PushBack_PROS/src/driftless/robot/subsystems/hood/DirectHoodBuilder.cpp
+++ Driftless_PushBack_PROS/src/driftless/robot/subsystems/hood/DirectHoodBuilder.cpp
@@ -1,40 +1,52 @@
 #include "driftless/robot/subsystems/hood/DirectHoodBuilder.hpp"
 
 namespace driftless::robot::subsystems::hood {
-  DirectHoodBuilder* DirectHoodBuilder::withMotor(std::unique_ptr<io::IMotor>& motor) {
-    m_motors.addMotor(motor);
-    return this;
-  }
-
-  DirectHoodBuilder* DirectHoodBuilder::withHeightPiston(std::unique_ptr<io::IPiston>& piston) {
-    m_height_pistons.addPiston(piston);
-    return this;
-  }
-
-  DirectHoodBuilder* DirectHoodBuilder::withGatePiston(std::unique_ptr<io::IPiston>& piston) {
-    m_gate_pistons.addPiston(piston);
-    return this;
-  }
-
-  DirectHoodBuilder* DirectHoodBuilder::withDescorePiston(std::unique_ptr<io::IPiston>& piston) {
-    m_descore_pistons.addPiston(piston);
-    return this;
-  }
-
-  DirectHoodBuilder* DirectHoodBuilder::withBumpPiston(std::unique_ptr<io::IPiston>& piston) {
-    m_bump_pistons.addPiston(piston);
-    return this;
-  }
-
-  std::unique_ptr<IHood> DirectHoodBuilder::build() {
-    std::unique_ptr<DirectHood> direct_hood{std::make_unique<DirectHood>()};
-
-    direct_hood->setMotors(m_motors);
-    direct_hood->setHeightPistons(m_height_pistons);
-    direct_hood->setGatePistons(m_gate_pistons);
-    direct_hood->setDescorePistons(m_descore_pistons);
-    direct_hood->setBumpPistons(m_bump_pistons);
-
-    return direct_hood;
-  }
+DirectHoodBuilder* DirectHoodBuilder::withMotor(
+    std::unique_ptr<io::IMotor>& motor) {
+  m_motors.addMotor(motor);
+  return this;
 }
+
+DirectHoodBuilder* DirectHoodBuilder::withHeightPiston(
+    std::unique_ptr<io::IPiston>& piston) {
+  m_height_pistons.addPiston(piston);
+  return this;
+}
+
+DirectHoodBuilder* DirectHoodBuilder::withGatePiston(
+    std::unique_ptr<io::IPiston>& piston) {
+  m_gate_pistons.addPiston(piston);
+  return this;
+}
+
+DirectHoodBuilder* DirectHoodBuilder::withLowerDescorePiston(
+    std::unique_ptr<io::IPiston>& piston) {
+  m_lower_descore_pistons.addPiston(piston);
+  return this;
+}
+
+DirectHoodBuilder* DirectHoodBuilder::withUpperDescorePiston(
+    std::unique_ptr<io::IPiston>& piston) {
+  m_upper_descore_pistons.addPiston(piston);
+  return this;
+}
+
+DirectHoodBuilder* DirectHoodBuilder::withBumpPiston(
+    std::unique_ptr<io::IPiston>& piston) {
+  m_bump_pistons.addPiston(piston);
+  return this;
+}
+
+std::unique_ptr<IHood> DirectHoodBuilder::build() {
+  std::unique_ptr<DirectHood> direct_hood{std::make_unique<DirectHood>()};
+
+  direct_hood->setMotors(m_motors);
+  direct_hood->setHeightPistons(m_height_pistons);
+  direct_hood->setGatePistons(m_gate_pistons);
+  direct_hood->setLowerDescorePistons(m_lower_descore_pistons);
+  direct_hood->setUpperDescorePistons(m_upper_descore_pistons);
+  direct_hood->setBumpPistons(m_bump_pistons);
+
+  return direct_hood;
+}
+}  // namespace driftless::robot::subsystems::hood
