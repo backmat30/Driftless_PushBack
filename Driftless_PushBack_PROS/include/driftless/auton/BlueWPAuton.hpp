87f718ca0d28898aa5f47b64928194a627d06237
Pufferfishking <43255657+Pufferfishking@users.noreply.github.com>
2026-03-02 14:58:35 -0600

    Bugfix tune blue win point auton (#40)
    * Finished tuning blue win point auto

--- Driftless_PushBack_PROS/include/driftless/auton/BlueWPAuton.hpp
+++ Driftless_PushBack_PROS/include/driftless/auton/BlueWPAuton.hpp
@@ -1,5 +1,5 @@
-#ifndef __BLUE_CENTER_AUTON_HPP__
-#define __BLUE_CENTER_AUTON_HPP__
+#ifndef __BLUE_WIN_POINT_AUTON_HPP__
+#define __BLUE_WIN_POINT_AUTON_HPP__
 
 #include "driftless/auton/AAuton.hpp"
 
@@ -11,78 +11,61 @@ namespace driftless {
 /// @author Matthew Backman
 namespace auton {
 
-/// @brief Class representing the autonomous routine for the blue robot's center match auton
+/// @brief Class representing the autonomous routine for the blue robot's win point auton
 /// @author Ethan jacobson
-class BlueCenterAuton : public AAuton {
+class BlueWPAuton : public AAuton {
  private:
   static constexpr double MAX_VELOCITY{80.0};
 
-  control::Point matchload_lineup{26.0, 26.0, M_PI / 2.0};
+  // matchload points
+  control::Point matchload_lineup{26.0, 22.0, M_PI / 2.0};
 
-  control::Point matchload{26, 11.5, M_PI / 2.0};
+  control::Point matchload_lineup_2{29.0, 22.0, M_PI / 2.0};
 
-  control::Point matchload_2{27, 11.5, M_PI / 2.0};
+  control::Point matchload{26.0, 11.0, M_PI / 2.0};
 
-  control::Point long_goal_lineup{28.0, 26.0, M_PI / 4.0};
+  control::Point matchload_2{28.0, 11.5, M_PI / 2.0};
 
-  control::Point long_goal_lineup_2{28.0, 31.0, M_PI / 4.0};
+  // long goal points
+  control::Point long_goal{27.5, 42.0, M_PI / 2.0};
 
-  control::Point long_goal{26, 42.0, M_PI / 2.0};
-
-  control::Point long_goal_to_matchload{12.0, 12.0, M_PI / 2.0};
-
-  control::Point descore_lineup{14.0, 38.0, M_PI / 2.0};
-
-  control::Point start_descore{16.0, 45.0, M_PI / 2.0};
+  // descore points
+  control::Point descore_lineup{15.5, 37.0, M_PI / 2.0};
   
   control::Point end_descore{16.0, 58.0, M_PI / 2.0};
 
-  control::Point center_two_blocks_lineup{12.0, 50.0, M_PI / 5.5};
+  // two blocks points
+  control::Point two_blocks_lineup{12.0, 56.0, 0.0};
 
-  control::Point center_two_blocks{21.0, 61.0, M_PI / 4.0};
+  control::Point two_blocks_lineup_2{23.0, 59.0, M_PI / 4.0};
 
-  control::Point center_two_blocks_2{24, 62.0, M_PI / 2.5};
+  control::Point two_blocks{29.0, 62.0, M_PI / 2.0};
 
-  control::Point center_two_blocks_3{26.0, 64.0, M_PI / 2};
+  // line block points
+  control::Point line_blocks_lineup{30.0, 62.5, M_PI / 4.0};
 
-  control::Point center_six_blocks_lineup{30.0, 62.0, M_PI / 2};
+  control::Point line_blocks_lineup_2{32.0, 67, M_PI / 4.0};
 
-  control::Point center_six_blocks{32.0, 67.5, M_PI / 4};
+  control::Point line_blocks{49.0, 66.0, M_PI / 4.0};
 
-  control::Point center_six_blocks_2{41.5, 70, M_PI / 6};
-
-  control::Point center_six_blocks_3{51.5, 72.0, M_PI / 5};
-
-  control::Point bottom_goal_lineup{54.0, 78.0, 0.0};
-
-  control::Point bottom_goal{58.0, 83.0, 0};
-
+  // middle goal points
   control::Point bottom_goal_1{58.0, 85.0, 0};
 
-  control::Point bottom_goal_2{76, 76, - M_PI / 8};
-
-  control::Point matchload_lineup_2{38, 38.0, M_PI / 2};
+  control::Point bottom_goal_2{78, 76, - M_PI / 6};
 
   control::Point top_goal_lineup{51, 48, M_PI / 4};
 
-  control::Point top_goal{61.5, 58.5, M_PI / 4};
+  control::Point top_goal{62.25, 59.25, M_PI / 4};
 
-//   control::Point park_lineup{44, 20, 0};
+  // other side descore
+  control::Point descore_2_lineup{50.0, 50.0, M_PI / 4.0};
 
-//   control::Point park_lineup_2{46.0, 10.0, 0.0};
+  control::Point descore_2{38.5, 38.0, M_PI / 2};
 
-//   control::Point park_zone{80.0, 6.0, 0.0};
-
-  control::Point park_lineup{52.0, 12.0, 0};
-
-  control::Point park_lineup_2{45.0, 10.0, 0.0};
-
-  control::Point park_zone{80.0, 5.0, 0.0};
-
-
+  control::Point descore_2_end{38.0, 61.0, M_PI / 2};
  public:
   /// @brief Constructs a new blue pump n dump auton object
-  BlueCenterAuton();
+  BlueWPAuton();
 
   /// @brief Initializes the auton
   /// @param robot __std::shared_ptr<robot::Robot>&__ The robot being controlled
---
+++ Driftless_PushBack_PROS/src/driftless/auton/BlueWPAuton.cpp
@@ -0,0 +1,157 @@
+#include "driftless/auton/BlueWPAuton.hpp"
+
+namespace driftless::auton {
+BlueWPAuton::BlueWPAuton() : AAuton("Blue_Win_Point") {}
+
+void BlueWPAuton::init(
+    std::shared_ptr<driftless::robot::Robot>& robot,
+    std::shared_ptr<driftless::control::ControlSystem>& control_system) {
+  m_robot = robot;
+  m_control_system = control_system;
+}
+
+void BlueWPAuton::run(
+    std::shared_ptr<driftless::robot::Robot>& robot,
+    std::shared_ptr<driftless::control::ControlSystem>& control_system,
+    std::shared_ptr<driftless::alliance::IAlliance>& alliance,
+    std::shared_ptr<rtos::IClock>& clock,
+    std::unique_ptr<rtos::IDelayer>& delayer) {
+  m_robot = robot;
+  m_control_system = control_system;
+  m_alliance = alliance;
+  m_clock = clock;
+  m_delayer = delayer->clone();
+
+  uint32_t start_time{getTime()};
+  setOdomPosition(56.0, 20.0, 0.0);
+  startColorSort(m_alliance->getAlliance());
+  intakeFront();
+
+  // go to matchloader
+  intakeFront();
+  goToPoint(matchload_lineup, MAX_VELOCITY);
+  delay(750);
+  setGoToPointVelocity(MAX_VELOCITY / 1.5);
+  delay(200);
+  goToPoint(matchload, MAX_VELOCITY / 1.5);
+  waitForGoToPoint(matchload, 2.0, 1500);
+  deployBackIntakeArms();
+  intakeBackToHood();
+
+  delay(450);
+  hoodRaise();
+
+  delay(225);
+  retractBackIntakeArms();
+
+  // score blocks from match loader
+  goToPoint(long_goal, MAX_VELOCITY / 1.5);
+  delay(200);
+  outtakeFront();
+  delay(200);
+  intakeFront();
+  waitForGoToPoint(long_goal, 10.0, 1250);
+  intakeFront();
+
+  waitForGoToPoint(long_goal, 2.0, 750);
+  hoodOpenDoor();
+
+  delay(1000);
+
+  // go descore
+  goToPoint(descore_lineup, MAX_VELOCITY / 2.0);
+  middleDescore();
+  delay(100);
+  intakeStop();
+  waitForGoToPoint(descore_lineup, 1.0, 2000);
+
+  goToPoint(end_descore, MAX_VELOCITY / 3.0);
+  waitForGoToPoint(end_descore, 2.5, 3000);
+
+  // grab two blocks under goal
+  deployDescore();
+  goToPoint(two_blocks_lineup, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(two_blocks_lineup, 1.5, 2000);
+
+  hoodLower();
+  hoodCloseDoor();
+  retractDescore();
+  intakeFront();
+
+  goToPoint(two_blocks_lineup_2, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(two_blocks_lineup_2, 1.5, 2000);
+
+  goToPoint(two_blocks, MAX_VELOCITY / 3.0);
+  waitForGoToPoint(two_blocks, 1.0, 3000);
+
+  // go middle 6 blocks
+  goToPoint(line_blocks_lineup, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(line_blocks_lineup, 1.5, 2500);
+
+  goToPoint(line_blocks_lineup_2, MAX_VELOCITY / 5.0);
+  waitForGoToPoint(line_blocks_lineup_2, 1.0, 1000);
+
+  goToPoint(line_blocks, MAX_VELOCITY / 3.0);
+  waitForGoToPoint(line_blocks, 1.0, 3500);
+
+  // go bottom goal
+  goToPoint(bottom_goal_1, MAX_VELOCITY);
+  waitForGoToPoint(bottom_goal_1, 5.0, 1750);
+  goToPoint(bottom_goal_2, MAX_VELOCITY / 2);
+  waitForGoToPoint(bottom_goal_2, 1.0, 400);
+  delay(100);
+
+  hoodSetVoltage(-10.0);
+  outtakeFront();
+
+  delay(2300);
+
+  // go matchload again
+  intakeStop();
+  goToPoint(matchload_lineup_2, MAX_VELOCITY);
+  waitForGoToPoint(matchload_lineup_2, 4, 3500);
+  goToPoint(matchload_2, MAX_VELOCITY / 1.5);
+  waitForGoToPoint(matchload_2, 2.0, 1250);
+  deployBackIntakeArms();
+  intakeBack();
+
+  delay(2000);
+
+  // go top goal
+  goToPoint(top_goal_lineup, MAX_VELOCITY);
+  delay(200);
+  retractBackIntakeArms();
+  intakeStop();
+  waitForGoToPoint(top_goal_lineup, 3.0, 2000);
+  hoodBumpUp();
+  goToPoint(top_goal, MAX_VELOCITY / 3);
+  waitForGoToPoint(top_goal, 2.0, 2000);
+  hoodOpenDoor();
+  intakeFront();
+  hoodSetVoltage(6.0);
+  
+
+  delay(1000);
+  hoodSetVoltage(9.0);
+  delay(1000);
+  hoodRaise();
+
+  // go descore
+  goToPoint(descore_2_lineup, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(descore_2_lineup, 1.0, 2000);
+
+  middleDescore();
+
+  goToPoint(descore_2, MAX_VELOCITY / 2.0);
+  waitForGoToPoint(descore_2, 1.0, 2000);
+
+  goToPoint(descore_2_end, MAX_VELOCITY / 3.0);
+  waitForGoToPoint(descore_2_end, 1.0, 2000);
+
+  stopMotion();
+
+  // leave at end
+  pros::screen::print(pros::E_TEXT_LARGE_CENTER, 8, "Runtime: %7.2f",
+                      (getTime() - start_time) / 1000.0);
+}
+}  // namespace driftless::auton
