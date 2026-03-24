a4df5451ec61bface75238054c96575f91408adc
backmat30 <matthewbackman@outlook.com>
2026-03-24 17:35:37 -0500

    Update xDriveModule to use motor velocity control rather than motor voltages

--- Driftless_PushBack_PROS/src/driftless/robot/subsystems/holonomic_drive_train/holonomic_drive_module/XDriveModule.cpp
+++ Driftless_PushBack_PROS/src/driftless/robot/subsystems/holonomic_drive_train/holonomic_drive_module/XDriveModule.cpp
@@ -31,16 +31,18 @@ void XDriveModule::setNormalizedMotionVector(
   // Calculate the y-component of the vector, aka forward velocity of the wheel
   double linear_velocity = std::sin(m_angle_offset) * motion_vector.y +
                            std::cos(m_angle_offset) * motion_vector.x;
-  double linear_voltage = linear_velocity * 12.0 * std::sqrt(2);
+  double linear_motor_velocity =
+      linear_velocity * std::sqrt(2) * (3600 / m_motors.getGearRatio());
 
   // calculate the velocity contribution from angular velocity
-  double turn_voltage = motion_vector.angular_velocity * -12.0;
+  double turn_motor_velocity =
+      motion_vector.angular_velocity * -(3600 / m_motors.getGearRatio());
 
   // Set the motor speeds (assuming a simple proportional control for
   // demonstration)
-  double module_voltage = linear_voltage + turn_voltage;
+  double module_velocity = linear_motor_velocity + turn_motor_velocity;
 
-  m_motors.setVoltage(module_voltage);
+  m_motors.setVelocity(module_velocity);
 }
 
 void XDriveModule::setRawVoltage(double voltage) {
