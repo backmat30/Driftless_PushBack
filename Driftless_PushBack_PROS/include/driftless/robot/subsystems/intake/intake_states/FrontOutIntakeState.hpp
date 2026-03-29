044ec30181d936e6111b4cead3edd19b74888404
backmat30 <matthewbackman@outlook.com>
2026-03-29 18:22:23 -0500

    Added a state for running the intake out the front

---
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/intake/intake_states/FrontOutIntakeState.hpp
@@ -0,0 +1,41 @@
+#ifndef __FRONT_OUT_INTAKE_STATE_HPP__
+#define __FRONT_OUT_INTAKE_STATE_HPP__
+
+#include "driftless/robot/subsystems/intake/intake_states/IIntakeState.hpp"
+
+/// @brief The namespace for driftless library code
+/// @author Matthew Backman
+namespace driftless {
+
+/// @brief The namespace for robot code
+/// @author Matthew Backman
+namespace robot {
+
+/// @brief The namespace for subsystems code
+/// @author Matthew Backman
+namespace subsystems {
+
+/// @brief Namespace for the intake subsystem
+/// @author Matthew Backman
+namespace intake {
+
+/// @brief Namespace for the various states the intake can be in while running
+/// @author Matthew Backman
+namespace intake_states {
+
+/// @brief Intake state for running the front intake motors backwards
+/// @author Matthew Backman
+class FrontOutIntakeState : public IIntakeState {
+ public:
+  /// @brief Updates the intake to run the front intake motors forward
+  /// @param intake __StateMachineIntake&__ The intake being updated
+  void update(StateMachineIntake& intake) override;
+};
+
+}  // namespace intake_states
+}  // namespace intake
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace driftless
+
+#endif
