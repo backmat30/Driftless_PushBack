bfd5ab331cca03d15485162911ae5ce49ef7cd73
backmat30 <matthewbackman@outlook.com>
2026-03-29 14:53:55 -0500

    Added an enum for the states of the intake

---
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/intake/intake_states/EIntakeStates.hpp
@@ -0,0 +1,42 @@
+#ifndef __E_INTAKE_STATES_HPP__
+#define __E_INTAKE_STATES_HPP__
+
+/// @brief The namespace for driftless library code
+/// @author Matthew Backman
+namespace driftless {
+
+/// @brief The namespace for robot code
+/// @author Matthew Backman
+namespace robot {
+
+/// @brief The namespace for subsystems code
+/// @author Matthew Backman
+namespace subsystems {
+
+/// @brief Namespace for the intake subsystem
+/// @author Matthew Backman
+namespace intake {
+
+/// @brief Namespace for the various states the intake can be in while running
+/// @author Matthew Backman
+namespace intake_states {
+
+/// @brief Enum containing the various states the intake can be in
+/// @author Matthew Backman
+enum class EIntakeStates {
+  IDLE,
+  FRONT_IN,
+  FRONT_OUT,
+  BACK_IN_BOTTOM_WAITING,
+  BACK_IN_BOTTOM_FIRST_BLOCK,
+  BACK_IN_BOTTOM_SECOND_BLOCK,
+  BACK_IN_TOP,
+  EJECT_BLOCK
+};
+}  // namespace intake_states
+}  // namespace intake
+}  // namespace subsystems
+}  // namespace robot
+}  // namespace driftless
+
+#endif
