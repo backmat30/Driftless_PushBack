06b165559d3d7566c1d4ea0e23d04de5bd368e9a
backmat30 <matthewbackman@outlook.com>
2026-03-29 15:26:57 -0500

    Added documentation to intake state methods

--- Driftless_PushBack_PROS/include/driftless/robot/subsystems/intake/intake_states/IIntakeState.hpp
+++ Driftless_PushBack_PROS/include/driftless/robot/subsystems/intake/intake_states/IIntakeState.hpp
@@ -26,10 +26,18 @@ namespace intake_states {
 
 class IIntakeState {
  public:
+  /// @brief Deletes the intake state object
   virtual ~IIntakeState() = default;
 
+  /// @brief Updates the intake using the state's behaviour
+  /// @param intake __StateMachineIntake&__ The intake being updated
   virtual void update(StateMachineIntake& intake) = 0;
 
+  /// @brief Checks if the intake should transition to another state, and
+  /// returns the state to transition to if so
+  /// @param intake __StateMachineIntake&__ The intake being checked
+  /// @return __EIntakeStates__ The state to transition to, or the current state
+  /// if no transition should occur
   virtual EIntakeStates processStateChange(StateMachineIntake& intake) = 0;
 };
 
