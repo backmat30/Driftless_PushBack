346e6953c1834cbee11efb031bc9a4abd38200cd
backmat30 <matthewbackman@outlook.com>
2026-04-13 15:34:54 -0500

    Reworked back to bottom intake to use 3 color sensors

--- Driftless_PushBack_PROS/src/driftless/robot/subsystems/intake/intake_states/BackToBottomIntakeState.cpp
+++ Driftless_PushBack_PROS/src/driftless/robot/subsystems/intake/intake_states/BackToBottomIntakeState.cpp
@@ -4,32 +4,30 @@
 namespace driftless::robot::subsystems::intake::intake_states {
 void BackToBottomIntakeState::update(StateMachineIntake& intake) {
   // detect first block and record position
-  if (intake.hasAllianceBlock() && !m_has_first_matchloader_block) {
+  if (intake.hasAllianceBlock(StateMachineIntake::ColorSortLocation::BACK) &&
+      !m_has_first_matchloader_block) {
     m_first_matchloader_block_pos = intake.getFrontMotorPosition();
     m_has_first_matchloader_block = true;
-    pros::screen::print(pros::E_TEXT_MEDIUM_CENTER, 5, "Has block 1");
     // if we have the first block, are not yet ready for the second block, and
     // there is either no block visible or we have moved the front intake far
     // enough, declare that we are ready for block 2
   } else if (m_has_first_matchloader_block &&
              !m_ready_for_second_matchloader_block &&
-             intake.getFrontMotorPosition() <
-                 m_first_matchloader_block_pos - 3.0) {
+             intake.hasAllianceBlock(
+                 StateMachineIntake::ColorSortLocation::MID)) {
     m_ready_for_second_matchloader_block = true;
-    pros::screen::print(pros::E_TEXT_MEDIUM_CENTER, 6, "Ready for block 2");
     // if we are ready for a second block but do not have a second block,
     // check if we can see a second block and update flag
   } else if (m_ready_for_second_matchloader_block &&
-             !m_has_second_matchloader_block && intake.hasBlock()) {
+             !m_has_second_matchloader_block &&
+             intake.hasBlock(StateMachineIntake::ColorSortLocation::BACK)) {
     m_has_second_matchloader_block = true;
     m_second_matchloader_block_pos = intake.getFrontMotorPosition();
-    pros::screen::print(pros::E_TEXT_MEDIUM_CENTER, 7, "has block 2");
     // wait for the front intake to go far enough, then update the flag to
     // direct blocks to the hood
   } else if (m_has_second_matchloader_block &&
-             intake.getFrontMotorPosition() <
-                 m_second_matchloader_block_pos - 3.0) {
-    pros::screen::print(pros::E_TEXT_MEDIUM_CENTER, 8, "Bottom full");
+             intake.hasAllianceBlock(
+                 StateMachineIntake::ColorSortLocation::FRONT)) {
     m_has_first_matchloader_block = false;
     m_ready_for_second_matchloader_block = false;
     m_has_second_matchloader_block = false;
