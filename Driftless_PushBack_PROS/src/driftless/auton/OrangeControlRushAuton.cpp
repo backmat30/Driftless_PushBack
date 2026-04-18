49af8e6db6050893b1e22b59340522bfc668d91e
backmat30 <matthewbackman@outlook.com>
2026-04-18 15:50:10 -0500

    Updated orange 4 ball rush for worlds

--- Driftless_PushBack_PROS/src/driftless/auton/OrangeControlRushAuton.cpp
+++ Driftless_PushBack_PROS/src/driftless/auton/OrangeControlRushAuton.cpp
@@ -23,38 +23,32 @@ void OrangeControlRushAuton::run(
   m_delayer = delayer->clone();
 
   uint32_t start_time{getTime()};
-  setOdomPosition(91, 20.0, M_PI);
+  setOdomPosition(88.0, 20.0, M_PI);
   startColorSort(m_alliance->getAlliance());
 
   // ROUTE GOES HERE
   intakeFront();
 
-  // go to matchloader
-  goToPose(matchload_lineup, MAX_VELOCITY, MAX_ANGULAR_VELOCITY);
-  delay(750);
-  setGoToPoseVelocity(MAX_VELOCITY / 2.0);
-  waitForGoToPoint(matchload_lineup, 4.0, 1000);
-  goToPose(matchload, MAX_VELOCITY / 2.0, MAX_ANGULAR_VELOCITY);
-  waitForGoToPose(matchload, 2.0, 1500);
+  // go to front matchloader
+  goToPose(match_load_lineup, MAX_VELOCITY, MAX_ANGULAR_VELOCITY, 80.0);
+  waitForGoToPose(match_load_lineup, 2.0, 2000);
+  goToPose(match_load, MAX_VELOCITY, MAX_ANGULAR_VELOCITY, 20.0);
+  waitForGoToPose(match_load, 2.0, 2000);
+  intakeBack();
   deployBackIntakeArms();
-  intakeBackToHood();
 
   delay(450);
   hoodRaise();
-
-  delay(210);
-  intakeStop();
-  retractBackIntakeArms();
+  delay(300);
 
   // score blocks from match loader
-  goToPose(long_goal, MAX_VELOCITY / 1.25, MAX_ANGULAR_VELOCITY);
+  goToPose(long_goal, MAX_VELOCITY, MAX_ANGULAR_VELOCITY, 30.0);
   delay(100);
   intakeStop();
+  retractBackIntakeArms();
   delay(150);
   waitForGoToPose(long_goal, 12.0, 1000);
   intakeFront();
-
-  setGoToPoseVelocity(MAX_VELOCITY / 3.5);
   waitForGoToPose(long_goal, 2.0, 750);
   hoodOpenDoor();
 
@@ -69,20 +63,6 @@ void OrangeControlRushAuton::run(
 
   goToPose(end_descore, MAX_VELOCITY / 3.0, MAX_ANGULAR_VELOCITY);
   waitForGoToPose(end_descore, 2.5, 3000);
-
-  // go to block mid goal
-  deployDescore();
-  delay(100);
-
-  goToPoint(middle_goal_lineup, MAX_VELOCITY / 2.0);
-  waitForGoToPoint(middle_goal_lineup, 2.0, 2000);
-
-  retractDescore();
-  hoodLower();
-
-  goToPoint(middle_goal_block, MAX_VELOCITY / 2.0);
-  waitForGoToPoint(middle_goal_block, 2.0, 2000);
-
   stopMotion();
 
   // leave at end
